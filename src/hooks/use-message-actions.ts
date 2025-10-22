23a575ce50a2ed54ad22cb1055fedca561885f8f
Almudena Guerras De Diego <almudenaguerrasdediego@MacBook-Pro-de-Almudena-2.local>
2025-10-22 15:02:04 +0200

    ✨ feat: add function to send messages and update room's lastMessage

--- src/hooks/use-message-actions.ts
+++ src/hooks/use-message-actions.ts
@@ -1,8 +1,18 @@
 import type { Message } from "@/schemas/message.schema";
-import { collection, orderBy, query } from "firebase/firestore";
-import { useFirestore, useFirestoreCollectionData } from "reactfire";
+import type { LastMessage } from "@/schemas/room.schema";
+import {
+  addDoc,
+  collection,
+  doc,
+  orderBy,
+  query,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { useFirestore, useFirestoreCollectionData, useUser } from "reactfire";
 
 export const useMessageActions = (roomId: string) => {
+  const { data: user } = useUser();
   const db = useFirestore();
 
   const messagesRef = collection(db, "rooms", roomId, "messages");
@@ -14,7 +24,41 @@ export const useMessageActions = (roomId: string) => {
     idField: "id",
   });
 
+  const sendMessage = async (text: string) => {
+    if (!user)
+      throw new Error("useMessageActions: 401 - User not authenticated");
+
+    if (!text.trim())
+      throw new Error("useMessageActions: 400 - Message is empty");
+
+    const timestamp = serverTimestamp();
+
+    const messageData: Omit<Message, "id"> = {
+      senderId: user.uid,
+      text,
+      timestamp,
+    };
+
+    const roomRef = doc(db, "rooms", roomId);
+
+    const lastMessage: LastMessage = {
+      senderId: user.uid,
+      text,
+      timestamp,
+    };
+
+    await Promise.all([
+      // update the lastMessage field in the room document
+      updateDoc(roomRef, {
+        lastMessage,
+      }),
+      // add the message to the messages collection
+      addDoc(messagesRef, messageData),
+    ]);
+  };
+
   return {
     messages: messages as Message[],
+    sendMessage,
   };
 };
